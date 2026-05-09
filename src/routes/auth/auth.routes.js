cdeb4a36cb7b1c8152e90ee0c93283c18b75f8ef
N-hub2 <nalinazari21@gmail.com>
2026-05-09 22:50:34 +0200

    feat(auth): ajouter la connexion utilisateur

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -5,5 +5,6 @@ const authController = require('../../controllers/auth/auth.controller');
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
