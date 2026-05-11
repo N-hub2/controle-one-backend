4ba71eeaa3dffb3b3cc836f50328464960df20d3
N-hub2 <nalinazari21@gmail.com>
2026-05-11 20:50:22 +0200

    feat(reservations): ajouter mes réservations

--- src/routes/reservations/reservation.routes.js
+++ src/routes/reservations/reservation.routes.js
@@ -5,6 +5,7 @@ const { requireAuth, requireRole } = require('../../middlewares/auth.middleware'
 
 const router = express.Router();
 
+router.get('/me', requireAuth, requireRole('client'), reservationController.getMyReservations);
 router.post('/', requireAuth, requireRole('client'), reservationController.createReservation);
 
 module.exports = router;
