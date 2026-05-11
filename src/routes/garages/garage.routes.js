d9aa8a33f3ca2edc5407c570d4fd956763428277
N-hub2 <nalinazari21@gmail.com>
2026-05-11 10:27:28 +0200

    feat(garages): ajouter le détail d'un garage

--- src/routes/garages/garage.routes.js
+++ src/routes/garages/garage.routes.js
@@ -5,5 +5,6 @@ const garageController = require('../../controllers/garages/garage.controller');
 const router = express.Router();
 
 router.get('/list', garageController.listGarages);
+router.get('/:id', garageController.getGarageById);
 
 module.exports = router;
