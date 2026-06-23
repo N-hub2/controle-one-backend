51acb1cb1c044e287ddfd4ba6120d375775a63bd
Ali <nalinazari21@gmail.com>
2026-06-23 11:19:16 +0200

    fix: hide internal server error details

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,8 @@ const { errorResponse } = require('../utils/apiResponse');
 
 const errorMiddleware = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal server error';
+  const message =
+    statusCode >= 500 ? 'Internal server error' : err.message || 'Internal server error';
 
   return errorResponse(res, message, statusCode);
 };
